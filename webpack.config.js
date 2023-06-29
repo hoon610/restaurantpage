6cad24b5998093c35352a54c07447aa309cb8a7f
hoon610 <bhoonkang@gmail.com>
2023-06-28 21:02:40 -0400

    Fix webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-  entry: './src/index.js',
+    mode: 'none',
+    entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devServer: {
-    static: './dist',
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+  
 };
